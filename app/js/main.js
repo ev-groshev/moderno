87f16c91573ccf9edfd50f4a36c53e7916b2d9d5
Evgeny Groshev <webdev.yeisk@gmail.com>
2020-04-13 20:23:52 +0300

    added new-items

--- app/js/main.js
+++ app/js/main.js
@@ -12,5 +12,27 @@ $(function () {
 		slidesToShow: 4,
 		slidesToScroll: 4
 	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
+	$('.icon-th-list').on('click', function () {
+		$('.products__item').addClass('list');
+		$('.icon-th-list').addClass('active');
+		$('.products__item').removeClass('active');
+	});
+
+	$('.icon-th-large').on('click', function () {
+		$('.products__item').removeClass('list');
+		$('.products__item').addClass('active');
+		$('.icon-th-list').removeClass('active');
+	});
+
 	var mixer = mixitup('.products__inner-box');
 });
